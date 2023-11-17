63266fb788a5912e52630a1ddfb7eb5a436432cd
ARTHUR ALVES DE SOUSA <arthur.asousa@a.ucb.br>
2023-11-17 10:49:46 -0300

    'a'

---
+++ exemplo_sqlite/src/main/java/main/java/poo/model/Pessoa.java
@@ -0,0 +1,52 @@
+package main.java.poo.model;
+
+public class Pessoa {
+
+    private Long id;
+    private String nome;
+    private String email;
+
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+
+    public Pessoa(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+    
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    
+}
